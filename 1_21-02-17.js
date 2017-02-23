a4eebd7a9318f3244064ea0145c5b6d25a249f36
DanielIsai <driano@live.com.mx>
2017-02-23 13:50:54 -0600

    Update 1_21-02-17.js

--- 1_21-02-17.js
+++ 1_21-02-17.js
@@ -2,7 +2,7 @@
 var escena = new THREE.Scene();
 var camara = new THREE.PerspectiveCamera();
 camara.position.z=5;
-var render = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerHeight*.9, window.innerHeight*.9 );
 document.body.appendChild( renderer.domElement );
 renderer.render( escena, camara );

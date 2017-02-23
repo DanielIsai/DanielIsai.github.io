cab4b040ac51562c2fdba594bce4b0cb5ca336b3
DanielIsai <driano@live.com.mx>
2017-02-23 13:45:10 -0600

    Update 1_21-02-17.js

--- 1_21-02-17.js
+++ 1_21-02-17.js
@@ -1,34 +1,18 @@
-var forma = new THREE.Geometry();
-
-forma.vertices.push( new THREE.Vector3( 1,  0,  1 ) ); // Vértice 0
-forma.vertices.push( new THREE.Vector3( 1,  0, -1 ) ); // Vértice 1
-forma.vertices.push( new THREE.Vector3(-1,  0, -1 ) ); // Vértice 2
-forma.vertices.push( new THREE.Vector3(-1,  0,  1 ) ); // Vértice 3
-forma.vertices.push( new THREE.Vector3( 0,  1,  0 ) ); // Vértice 4
-
-forma.faces.push( new THREE.Face3( 3, 2, 1 ) ); // Cara 0
-forma.faces.push( new THREE.Face3( 3, 1, 0 ) ); // Cara 1
-forma.faces.push( new THREE.Face3( 3, 0, 4 ) ); // Cara 2
-forma.faces.push( new THREE.Face3( 0, 1, 4 ) ); // Cara 3
-forma.faces.push( new THREE.Face3( 1, 2, 4 ) ); // Cara 4
-forma.faces.push( new THREE.Face3( 2, 3, 4 ) ); // Cara 5
-
-forma.computeBoundingSphere();
-forma.computeFaceNormals();
-
-var material = new THREE.MeshNormalMaterial();
-
-var malla = new THREE.Mesh( forma, material );
-malla.rotateX(Math.PI/4);
-
+//Visualizar escena
 var escena = new THREE.Scene();
-escena.add( malla );
-
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z=5;
+var render = new THREE.WebGLRenderer();
+renderer.setSize( window.innerHeight*.9, window.innerHeight*.9 );
+document.body.appendChild( renderer.domElement );
+renderer.render( escena, camara );
 
-var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.95, 
-                      window.innerHeight*.95 );
-document.body.appendChild( renderizador.domElement );
-renderizador.render( escena, camara );
+//Definir figura
+var forma = new THREE.BoxGeometry( 1, 1, 1 );
+var cubo  = new THREE.Mesh( forma );
+cubo.rotation.x += 0.1;
+cubo.rotation.y += 0.1;
+
+//Agregar a la escena
+escena.add( cubo );
+renderer.render( escena, camara );

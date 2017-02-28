1acb7bf07836e3c9d63e8fc7b2d755147cae16e1
DanielIsai <driano@live.com.mx>
2017-02-28 14:22:01 -0600

    Update Personaje_2.js

--- Personaje_2.js
+++ Personaje_2.js
@@ -13,12 +13,13 @@ figura.lineTo( 0,  1,  5);
 
 var forma = new THREE.ShapeGeometry(figura);
 var malla = new THREE.Mesh(forma);
+malla.rotateX(Math.PI/4);
 
 var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 100;
+camara.position.z = 10;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95,

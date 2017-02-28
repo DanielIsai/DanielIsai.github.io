72fb44f0f73fc422a15bd72f023a6311850514ff
DanielIsai <driano@live.com.mx>
2017-02-27 20:42:10 -0600

    Update MV_Ejemplo1.js

--- MV_Ejemplo1.js
+++ MV_Ejemplo1.js
@@ -8,35 +8,12 @@ document.body.appendChild( renderer.domElement );
 renderer.render( escena, camara );
 
 //Definir figura
-
-  
-  /*Ejemplo Cubo
-  var forma = new THREE.BoxGeometry( 1, 1, 1 );
-  var cubo  = new THREE.Mesh( forma );
-  cubo.rotation.x += 0.1;
-  cubo.rotation.y += 0.1;*/
-
-
-var forma = new THREE.Geometry();
-forma.vertices.push( new THREE.Vector3( 1,  0,  1 ) ); // Vértice 0
-forma.vertices.push( new THREE.Vector3( 1,  0, -1 ) ); // Vértice 1
-forma.vertices.push( new THREE.Vector3(-1,  0, -1 ) ); // Vértice 2
-forma.vertices.push( new THREE.Vector3(-1,  0,  1 ) ); // Vértice 3
-forma.vertices.push( new THREE.Vector3( 0,  1,  0 ) ); // Vértice 4
-
-forma.faces.push( new THREE.Face3( 3, 2, 1 ) ); // Cara 0
-forma.faces.push( new THREE.Face3( 3, 1, 0 ) ); // Cara 1
-forma.faces.push( new THREE.Face3( 3, 0, 4 ) ); // Cara 2
-forma.faces.push( new THREE.Face3( 0, 1, 4 ) ); // Cara 3
-forma.faces.push( new THREE.Face3( 1, 2, 4 ) ); // Cara 4
-forma.faces.push( new THREE.Face3( 2, 3, 4 ) ); // Cara 5
-
-var material = new THREE.MeshNormalMaterial();
-var malla = new THREE.Mesh( forma, material );
-malla.rotateX(Math.PI/4);
-
+var forma = new THREE.BoxGeometry( 1, 1, 1 );
+var cubo  = new THREE.Mesh( forma );
+cubo.rotation.x += 0.1;
+cubo.rotation.y += 0.1;
 
 
 //Agregar a la escena
-escena.add( malla );
+escena.add( cubo);
 renderer.render( escena, camara );

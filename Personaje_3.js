d2ba774db4ba5bde277b046b2778bfba68b55289
DanielIsai <driano@live.com.mx>
2017-03-01 10:19:48 -0600

    Update Personaje_3.js

--- Personaje_3.js
+++ Personaje_3.js
@@ -14,7 +14,7 @@ escena.add(cuelloMalla);
 //escena.add(esferaMalla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.z = 50;
 
 renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );

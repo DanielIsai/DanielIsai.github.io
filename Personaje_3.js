1bf5752c7f6bd09c6ed94808a352fe6aeb731371
DanielIsai <driano@live.com.mx>
2017-03-01 16:52:42 -0600

    Update Personaje_3.js

--- Personaje_3.js
+++ Personaje_3.js
@@ -1,14 +1,22 @@
-var cuelloForma = new THREE.CylinderGeometry(1, 1, 7);
+var cuelloForma = new THREE.CylinderGeometry(1, 1, 10);
+var cuellForma = new THREE.CylinderGeometry(1, 1, 10);
 var cuerpoForma = new THREE.SphereGeometry(3);
+var baseForma = new THREE.CylinderGeometry(5, 5, 1);
+
+cuello2Forma.rotateX( Math.PI/4 );
 cuelloForma.translate(0,3,0);
 
 var cuelloMalla = new THREE.Mesh(cuelloForma);
+var cuello2Malla = new THREE.Mesh(cuello2Forma);
 var cuerpoMalla= new THREE.Mesh(cuerpoForma);
+var baseMalla= new THREE.Mesh(baseForma);
 
 var arbolForma = new THREE.Geometry();
 
 arbolForma.merge(cuelloMalla.geometry, cuelloMalla.matrix);
+arbolForma.merge(cuell2oMalla.geometry, cuello2Malla.matrix);
 arbolForma.merge(cuerpoMalla.geometry, cuerpoMalla.matrix);
+arbolForma.merge(baseMalla.geometry, baseMalla.matrix);
 
 var material = new THREE.MeshNormalMaterial();
 var arbolMalla = new THREE.Mesh(arbolForma, material);

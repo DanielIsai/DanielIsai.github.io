78bc01558b164dcee8a2f4535f95af2774e140c5
DanielIsai <driano@live.com.mx>
2017-03-01 23:08:10 -0600

    Update Personaje1.js

--- Personaje1.js
+++ Personaje1.js
@@ -1,44 +1,25 @@
-//Definir figura
+var puntos = [];
+for ( var i = 0; i < 50; i ++ ) {
+    puntos.push( new THREE.Vector2(
+                     Math.sin( i * 0.4 ) * 12 + 57,
+                     ( i - 6 ) * .9 ) );
+}
 
 
-var forma = new THREE.Geometry();
-
-forma.vertices.push( new THREE.Vector3( 0,  0,  0 ) ); // Vértice 0
-forma.vertices.push( new THREE.Vector3( 1,  0,  0 ) ); // Vértice 1
-forma.vertices.push( new THREE.Vector3( 1,  1,  0 ) ); // Vértice 2
-forma.vertices.push( new THREE.Vector3( 0,  1,  0 ) ); // Vértice 3
-
-forma.vertices.push( new THREE.Vector3( 0,  0,  1 ) ); // Vértice 4
-forma.vertices.push( new THREE.Vector3( 1,  0,  1 ) ); // Vértice 5
-forma.vertices.push( new THREE.Vector3( 1,  1,  1 ) ); // Vértice 6
-forma.vertices.push( new THREE.Vector3( 0,  1,  1 ) ); // Vértice 7
-
-
-
-forma.faces.push( new THREE.Face4( 0, 1, 2, 3 ) ); // Cara 0
-forma.faces.push( new THREE.Face4( 4, 5, 6, 7 ) ); // Cara 1
-/*forma.faces.push( new THREE.Face4( 0, 1, 5, 4 ) ); // Cara 2
-forma.faces.push( new THREE.Face4( 6, 7, 2, 3 ) ); // Cara 3
-forma.faces.push( new THREE.Face4( 5, 6, 1, 2 ) ); // Cara 4
-forma.faces.push( new THREE.Face4( 4, 7, 0, 3 ) ); // Cara 5 */
-
-forma.computeBoundingSphere();
-
-forma.computeFaceNormals();
-
+var forma = new THREE.LatheGeometry(puntos);
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
-malla.rotateX(Math.PI/4);
+malla.rotateX( Math.PI/6 );
 
-//Visualizar escena
 var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z=5;
+camara.position.z = 500;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerHeight*.9, window.innerHeight*.9 );
-
-document.body.appendChild( renderer.domElement );
-renderer.render( escena, camara );
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help

8bac68b7af0eb7d31f53fdcaf1b7cdaff711651e
DanielIsai <driano@live.com.mx>
2017-03-01 22:46:43 -0600

    Update Personaje_2.js

--- Personaje_2.js
+++ Personaje_2.js
@@ -61,7 +61,7 @@ var forma = new THREE.ExtrudeGeometry( figura,extrudeSettings);
 
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
-malla.rotateY( Math.PI/4 );
+malla.rotateY( Math.PI/6 );
 
 var escena = new THREE.Scene();
 escena.add(malla);
